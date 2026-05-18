3662584e1c3571c196a17dca20442a3da83e5f78
Daniel Ronalds <nzdanron@gmail.com>
2026-05-18 12:33:55 +1200

    refactor: hoist build_file_reference above is_valid_range

--- lua/reference/context.lua
+++ lua/reference/context.lua
@@ -1,11 +1,6 @@
 local M = {}
 
-local function is_valid_range(range)
-  return type(range) == 'table'
-    and type(range.line1) == 'number'
-    and type(range.line2) == 'number'
-    and range.line1 <= range.line2
-end
+local is_valid_range
 
 function M.build_file_reference(path, range)
   if path == nil or path == '' then
@@ -17,6 +12,13 @@ function M.build_file_reference(path, range)
   end
 
   return '@' .. path .. '#L' .. range.line1 .. '-' .. range.line2
+end
+
+function is_valid_range(range)
+  return type(range) == 'table'
+    and type(range.line1) == 'number'
+    and type(range.line2) == 'number'
+    and range.line1 <= range.line2
 end
 
 return M
