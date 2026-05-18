9fd35fd93177e7377c97b4e7a56c150d3520990c
Daniel Ronalds <nzdanron@gmail.com>
2026-05-18 12:26:43 +1200

    refactor: slim init.lua to config and setup

--- lua/reference/init.lua
+++ lua/reference/init.lua
@@ -1,6 +1,5 @@
 local M = {}
 
-local dispatch = require('reference.dispatch')
 local commands = require('reference.commands')
 
 local DEFAULT_CONFIG = {
@@ -10,14 +9,8 @@ local DEFAULT_CONFIG = {
   },
 }
 
-local config = vim.deepcopy(DEFAULT_CONFIG)
-
-function M._send(inputs)
-  dispatch.send(config, inputs)
-end
-
 function M.setup(opts)
-  config = vim.tbl_deep_extend('force', vim.deepcopy(DEFAULT_CONFIG), opts or {})
+  local config = vim.tbl_deep_extend('force', vim.deepcopy(DEFAULT_CONFIG), opts or {})
   commands.register(config)
 end
 
