102859530fd0a9b1c65d0bdf72e9938d4872c5a1
Daniel Ronalds <nzdanron@gmail.com>
2026-05-18 12:25:20 +1200

    refactor: move command wiring into reference.commands

--- lua/reference/init.lua
+++ lua/reference/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local dispatch = require('reference.dispatch')
+local commands = require('reference.commands')
 
 local DEFAULT_CONFIG = {
   tmux = {
@@ -17,24 +18,7 @@ end
 
 function M.setup(opts)
   config = vim.tbl_deep_extend('force', vim.deepcopy(DEFAULT_CONFIG), opts or {})
-
-  vim.api.nvim_create_user_command('ReferenceSend', function(cmd_opts)
-    local path = vim.fn.fnamemodify(vim.fn.expand('%:p'), ':~:.')
-    local range = nil
-    if cmd_opts.range == 2 then
-      range = { line1 = cmd_opts.line1, line2 = cmd_opts.line2 }
-    end
-    M._send({ path = path, range = range })
-  end, { range = true, force = true })
-
-  vim.api.nvim_create_user_command('ReferenceSendFirst', function(cmd_opts)
-    local path = vim.fn.fnamemodify(vim.fn.expand('%:p'), ':~:.')
-    local range = nil
-    if cmd_opts.range == 2 then
-      range = { line1 = cmd_opts.line1, line2 = cmd_opts.line2 }
-    end
-    M._send({ path = path, range = range, pick = 'first' })
-  end, { range = true, force = true })
+  commands.register(config)
 end
 
 return M
